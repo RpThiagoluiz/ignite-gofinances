8612f25fa24b44dc8f970c0e774c2af95a29b30b
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2022-02-02 18:28:08 -0300

    init

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@screens": "./src/screens",
+            "@components": "/components",
+            "@utils": "/utils",
+            "@store": "/store",
+          },
+          extensions: [
+            ".js",
+            ".jsx",
+            ".png",
+            ".svg",
+            ".json",
+            ".html",
+            ".pdf",
+            ".ts",
+            ".tsx",
+          ],
+        },
+      ],
+    ],
   };
 };
